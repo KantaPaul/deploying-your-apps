934dc8c49229fb448c2fccf1c11ec782be623582
KantaPaul <pobon.paul1994@gmai.com>
2018-08-03 00:20:08 +0600

    server setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ let path = require('path');
 let express = require('express');
 let app = express();
 let publicPath = path.join(__dirname, '..', 'public');
+let port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -9,6 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('server js is up')
+app.listen(port, () => {
+  console.log('Server js is up')
 })
