6c04dda47c45afa0b9a114342017967bd603f02d
KantaPaul <pobon.paul1994@gmai.com>
2018-08-02 23:46:27 +0600

    update

---
+++ server/server.js
@@ -0,0 +1,14 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+let publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('server js is up')
+})
